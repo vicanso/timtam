709fbfae7788447943e47a88df72808a6632b026
vicanso <vicansocanbico@gmail.com>
2015-10-27 21:06:49 +0800

    first commit

---
+++ routes/page.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = [{
+  route: '/angular',
+  template: 'angular/home',
+  handler: 'home'
+}, {
+  route: '/react',
+  template: 'react/home',
+  handler: 'home'
+}];
